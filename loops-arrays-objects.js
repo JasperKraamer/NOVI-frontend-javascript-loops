777f22a6049a152f79529bbba9f97f81211efcd0
JasperKraamer <jasperkraamer@gmail.com>
2024-04-15 01:42:09 +0200

    Huiswerk les2 Javascript loops

--- loops-arrays-objects.js
+++ loops-arrays-objects.js
@@ -16,7 +16,10 @@ const scores = [
 // 66
 // ==========================================
 
+for (let i = 0; i < scores.length; i++) {
+    console.log(scores[i].score)
 
+}
 
 
 
@@ -32,6 +35,23 @@ const scores = [
 // D
 // ==========================================
 
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i].score <60){
+        console.log('F')
+    }
+    else if (scores[i].score <70){
+        console.log('D')
+    }
+    else if (scores[i].score <80){
+        console.log('C')
+    }
+    else if (scores[i].score <90){
+        console.log('B')
+    }
+    else if (scores[i].score <100){
+        console.log('A')
+    }
+}
 
 
 
@@ -49,13 +69,32 @@ const scores = [
 //  ];
 // ==========================================
 
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i].score <60){
+        scores[i].grade = 'F'
+    }
+    else if (scores[i].score <70){
+        scores[i].grade = 'D'
+    }
+    else if (scores[i].score <80){
+        scores[i].grade = 'C'
+    }
+    else if (scores[i].score <90){
+        scores[i].grade = 'B'
+    }
+    else if (scores[i].score <100){
+        scores[i].grade = 'A'
+    }
+}
 
+console.log(scores)
 
 
 
 // ==========================================
 // Opdracht 2
-// Schrijf een script die e-mailadressen genereert voor al onze medewerkers. Sla dit op in een nieuwe property "email" die je toevoegt aan iedere medewerker.
+// Schrijf een script die e-mailadressen genereert voor al onze medewerkers. Sla dit op in een nieuwe property "email"
+// die je toevoegt aan iedere medewerker.
 
 const NOVIEmployees = [
     {firstName: 'Nova', lastName: 'Eeken'},
@@ -75,7 +114,10 @@ const NOVIEmployees = [
 //  ];
 // ==========================================
 
-
+for (let i = 0; i < NOVIEmployees.length; i++) {
+    NOVIEmployees[i].email = NOVIEmployees[i].firstName + '.' + NOVIEmployees[i].lastName + '@novi.nl'
+}
+console.log(NOVIEmployees)
 
 
 
@@ -83,7 +125,10 @@ const NOVIEmployees = [
 // Opdracht 2-BONUS
 // Lukt het je om ervoor te zorgen dat alle e-mailadressen in lowercase letters komen te staan? Dit heb je nog niet geleerd, maar google is your best friend...
 // ==========================================
-
+for (let i = 0; i < NOVIEmployees.length; i++) {
+    NOVIEmployees[i].email = NOVIEmployees[i].email.toLowerCase()
+}
+console.log(NOVIEmployees)
 
 
 
@@ -132,4 +177,29 @@ const students = [
 // ==========================================
 
 
-
+for (let i = 0; i < students.length; i++) {
+    switch (students[i].zipCode) {
+        case    '3513':
+            students[i].neighborhood = 'Pijlsweerd'
+            break;
+        case    '3514':
+            students[i].neighborhood = 'vogelenbuurt'
+            break;
+        case    '3512':
+            students[i].neighborhood = 'Binnenstad'
+            break;
+        case   '3531':
+            students[i].neighborhood = 'Lombok'
+            break;
+        case    '3572':
+            students[i].neighborhood = 'Wittevrouwen'
+            break;
+        case    '3581':
+            students[i].neighborhood = 'Oudwijk'
+            break;
+        case    '3583':
+            students[i].neighborhood = 'Schildersbuurt'
+            break;
+    }
+    console.log(students)
+}
